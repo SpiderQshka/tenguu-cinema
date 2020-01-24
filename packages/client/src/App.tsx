f95b3d91c52827718500faa17ad221e2fdaf4132
Leonid Prusov <39962817+SpiderQshka@users.noreply.github.com>
2020-01-24 20:23:43 +0300

    sending data from server to client, processing data on client side

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.sass";
 
+interface ResponseInterface {
+  greeting: string;
+}
+
+const URL = "http://localhost:4000";
+
+const loadData = async (url: string): Promise<ResponseInterface> => {
+  return fetch(url).then(
+    (response: Response): Promise<ResponseInterface> => response.json()
+  );
+};
+
 const App: React.FC = () => {
-  return <h1 className={styles.App}>Hello World!</h1>;
+  let [data, setData] = useState<ResponseInterface>({ greeting: "" });
+  loadData(URL).then((value: ResponseInterface) => setData(value));
+  return <h1 className={styles.App}>{data.greeting}</h1>;
 };
 
 export default App;
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,26 +1,32 @@
 import e from "express";
-import mongoose from "mongoose";
+import cors from "cors";
+// import mongoose from "mongoose";
 
-const main = async (): Promise<void> => {
-  await mongoose.connect(
-    "mongodb+srv://SpiderQshka:Prusov.2002@cluster0-phrn3.mongodb.net/test?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  );
-};
+// const start = async (): Promise<void> => {
+//   try {
+//     await mongoose.connect(
+//       "mongodb+srv://SpiderQshka:Prusov.2002@cluster0-phrn3.mongodb.net/test",
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//       }
+//     );
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
 
-main();
+// start();
 
 const app = e();
 const PORT = 4000;
-app.get("/", (req: e.Request, res: e.Response) => {
-  res.send("Hello, world");
+
+app.use(cors());
+
+app.use("/", (req, res) => {
+  res.json({ greeting: "Hello, World!" });
 });
 
 app.listen(PORT, () => {
   console.log(`server is running and using port: ${PORT}`);
 });
-
-export default app;
